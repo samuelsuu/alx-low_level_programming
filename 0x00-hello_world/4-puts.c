2e84bfbc0cb84ab08dde03d414a3650c5b868ba3
samuelsuu <107080477+samuelsuu@users.noreply.github.com>
2023-09-07 20:15:45 +0100

    edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
