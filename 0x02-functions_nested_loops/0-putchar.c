8380345aeccc9bad8c9ebd3aab12889a5f5ba575
samuelsuu <107080477+samuelsuu@users.noreply.github.com>
2023-09-13 05:49:26 +0100

    alx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a newline
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("_putchar\n");
+    return (0);
+}
