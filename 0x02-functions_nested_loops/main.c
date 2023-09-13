2d2ba795273d2759b12c0023d820206747f09b19
samuelsuu <107080477+samuelsuu@users.noreply.github.com>
2023-09-13 06:34:11 +0100

    up

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _islower('o');
+    _putchar(r + '0'); // Should print 1
+    r = _islower(108);
+    _putchar(r + '0'); // Should print 1
+    r = _islower('c');
+    _putchar(r + '0'); // Should print 1
+    r = _islower('I');
+    _putchar(r + '0'); // Should print 0
+    r = _islower('s');
+    _putchar(r + '0'); // Should print 1
+    r = _islower(70);
+    _putchar(r + '0'); // Should print 0
+    r = _islower(20);
+    _putchar(r + '0'); // Should print 0
+    r = _islower('U');
+    _putchar(r + '0'); // Should print 0
+    r = _islower('n');
+    _putchar(r + '0'); // Should print 1
+    r = _islower(0);
+    _putchar(r + '0'); // Should print 0
+    r = _islower('H');
+    _putchar(r + '0'); // Should print 0
+
+    _putchar('\n');
+    return (0);
+}
