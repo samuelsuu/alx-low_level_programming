4c6443688474050d0cf58836bdac7629f9fe9bbd
samuelsuu <107080477+samuelsuu@users.noreply.github.com>
2023-09-13 06:10:16 +0100

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
+#include <unistd.h> // Include the necessary header for the write function
 
 /**
- * main - Entry point
- *
- * Description: Prints "_putchar" followed by a newline
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("_putchar\n");
-    return (0);
+    char *output = "_putchar\n"; // Define the desired output
+
+    // Use the write function to print the output to the standard output (stdout)
+    // File descriptor 1 represents stdout
+    write(1, output, 9); // 9 is the length of the output string
+
+    return (0); // Return 0 to indicate success
+}
+#include <stdio.h>
+#include <unistd.h> // Include the necessary header for the write function
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char *output = "_putchar\n"; // Define the desired output
+
+    // Use the write function to print the output to the standard output (stdout)
+    // File descriptor 1 represents stdout
+    write(1, output, 9); // 9 is the length of the output string
+
+    return (0); // Return 0 to indicate success
 }
